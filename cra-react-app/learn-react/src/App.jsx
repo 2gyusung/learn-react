dae60d7e190784cf4cb2eca1166f488436db00f2
이규성 <leegs005@naver.com>
2025-03-22 00:12:20 +0900

    리스트렌더링,이벤트헨들러,이벤트전파

--- cra-react-app/learn-react/src/App.jsx
+++ cra-react-app/learn-react/src/App.jsx
@@ -1,45 +1,43 @@
-// import { useState } from 'react'
 import './App.css'
-import CourseItem from './components/course/CourseItem'
+import CourseForm from './components/course/CourseForm';
 import CourseListCard from './components/course/CourseListCard'
-import Footer from './components/Footer'
-import Headers from './components/Headers'
-import Main from './components/Main'
-
-
-
 function App() {
-  // const [count, setCount] = useState(0)
 
   const items = [
-      {
-        title: '입문자를 위한, HTML&CSS 웹 개발 입문',
-        description: '웹 개발에 필요한 기본 지식을 배웁니다.',
-        thumbnail: '/img/htmlcss.png',
-        isFavorite : true
-      },
-       {
-        title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
-        description: '쉽고! 알찬! 내용을 준비했습니다.',
-        thumbnail: '/img/js.png',
-        isFavorite : false,
-        link: 'https://inf.run/Kpnd'
-      },
-       {
-        title: '포트폴리오 사이트 만들고 배포까지!',
-        description: '포트폴리오 사이트를 만들고 배포해 보세요.',
-        thumbnail: '/img/portfolio.png',
-        isFavorite : true,
-        link: 'https://inf.run/YkAN'
-      }
-    
+    {
+      id: 0,
+      title: '입문자를 위한, HTML&CSS 웹 개발 입문',
+      description: '웹 개발에 필요한 기본 지식을 배웁니다.',
+      thumbnail: '/img/htmlcss.png',
+      isFavorite: false,
+      link: 'https://inf.run/JxyyT'
+    },
+    {
+      id: 1,
+      title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
+      description: '쉽고! 알찬! 내용을 준비했습니다.',
+      thumbnail: '/img/js.png',
+      isFavorite: true,
+      link: 'https://inf.run/Kpnd'
+    },
+    {
+      id: 2,
+      title: '포트폴리오 사이트 만들고 배포까지!',
+      description: '포트폴리오 사이트를 만들고 배포해 보세요.',
+      thumbnail: '/img/portfolio.png',
+      isFavorite: true,
+      link: 'https://inf.run/YkAN'
+    }
   ]
 
+  // const favoriteItems = items.filter(item => item.isFavorite);
   return (
     <>
-      <main>
-		<CourseListCard items={items}/>
-			</main>
+      <main style={{flexDirection: 'column', gap: '1rem'}}>
+        <CourseForm />
+        <CourseListCard title="강의 목록" items={items} />
+        {/* <CourseListCard title="관심 강의" items={favoriteItems} /> */}
+      </main>
     </>
   )
 }
--- cra-react-app/learn-react/src/components/course/CourseItem.jsx
+++ cra-react-app/learn-react/src/components/course/CourseItem.jsx
@@ -32,14 +32,10 @@
 // }
 
 
-function HeartIconBtn({ isFavorite = false }) {
+function HeartIconBtn({ onClick, isFavorite = false }) {
+
   return (
-    <button className="btn">
-      {/* {isFavorite ? (
-        <img className="btn__img" src="/img/heart-fill-icon.svg" />
-      ) : (
-        <img className="btn__img" src="/img/heart-icon.svg" />
-      )} */}
+    <button className="btn" onClick={onClick}>
       <img className="btn__img" src={isFavorite ? '/img/heart-fill-icon.svg' : '/img/heart-icon.svg'} />
     </button>
   )
@@ -47,26 +43,32 @@ function HeartIconBtn({ isFavorite = false }) {
 
 function LinkIconBtn({ link }) {
   return (
-	  <a className="btn" href={link} target="_blank" rel="noreferrer">
-	    <img className="btn__img" src="/img/link-icon.svg" />
-	  </a>
-	)
+    <a className="btn" href={link} target="_blank" rel="noreferrer">
+      <img className="btn__img" src="/img/link-icon.svg" />
+    </a>
+  )
 }
 
-
-export default function CourseItem({ title, description, thumbnail, isFavorite, link}) {
+export default function CourseItem({ title, description, thumbnail, isFavorite, link }) {
+  function handleFavorite(e) {
+    e.stopPropagation();
+    alert(isFavorite ? '좋아요' : '모르겠어요.');
+  }
+    function handleItemClick(e) {
+        e.stopPropagation();
+        alert('item click~!');
+        open(link, '_blank');
+     }
   return (
-    <article className="course">
+    <article className="course" onClick={handleItemClick}>
       <img className="course__img" src={thumbnail} alt="강의 이미지" />
       <div className="course__body">
         <div className="course__title">{title}</div>
         <div className="course__description">{description}</div>
       </div>
       <div className="course__icons">
-        <HeartIconBtn isFavorite={isFavorite} />
-        <div className="course__icons">
+        <HeartIconBtn isFavorite={isFavorite} onClick={handleFavorite} />
         {link && <LinkIconBtn link={link} />}
-      </div>
       </div>
     </article>
   );
